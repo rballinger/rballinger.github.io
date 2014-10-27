491de8833c20b6c48daecdc6a7488bedd71e6b6f
RyBall <rye5bread5@aol.com>
2014-10-26 21:29:41 -0400

    added angular with partials

---
+++ scripts/WebsiteApp.js
@@ -0,0 +1,15 @@
+var app = angular.module("websiteApp", []).
+  config(function($routeProvider){
+    $routeProvider.
+      when('/about', {template:'partials/about.html'}).
+      when('/pastprojects', {template: 'partials/pastprojects.html'}).
+      when('/currentprojects', {template: 'partials/currentprojects.html'}).
+      when('/futureprojects', {template: 'partials/futureprojects.html'}).
+      when('/skills', {template: 'partials/skills.html'}).
+      when('/algorithms', {template: 'partials/algorithms.html'}).
+      otherwise({redirectTo: '/about', template:'partials/about.html'})
+  });
+
+app.controller("ViewController", ["$scope", function($scope){
+
+}]);
