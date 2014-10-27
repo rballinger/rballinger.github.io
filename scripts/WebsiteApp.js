d78ce7b286f42576179b0826007f014ca2ad25c6
RyBall <rye5bread5@aol.com>
2014-10-26 22:26:42 -0400

    added 404 page

--- scripts/WebsiteApp.js
+++ scripts/WebsiteApp.js
@@ -1,15 +1,16 @@
-var app = angular.module("websiteApp", ['ngRoute']).
-  config(function($routeProvider, $locationProvider){
-    $routeProvider.
-      when('/', {templateUrl:'partials/about.html'}).
-      when('/pastprojects', {templateUrl: 'partials/pastprojects.html'}).
-      when('/currentprojects', {templateUrl: 'partials/currentprojects.html'}).
-      when('/futureprojects', {templateUrl: 'partials/futureprojects.html'}).
-      when('/skills', {templateUrl: 'partials/skills.html'}).
-      when('/algorithms', {templateUrl: 'partials/algorithms.html'}).
-      otherwise({redirectTo: '/'});
-      $locationProvider.html5Mode(true);
-  });
+var app = angular.module("websiteApp", ['ngRoute']);
+
+app.config(function($routeProvider, $locationProvider){
+  $routeProvider.
+    when('/', {templateUrl:'partials/about.html'}).
+    when('/pastprojects', {templateUrl: 'partials/pastprojects.html'}).
+    when('/currentprojects', {templateUrl: 'partials/currentprojects.html'}).
+    when('/futureprojects', {templateUrl: 'partials/futureprojects.html'}).
+    when('/skills', {templateUrl: 'partials/skills.html'}).
+    when('/algorithms', {templateUrl: 'partials/algorithms.html'}).
+    otherwise({redirectTo: '/'});
+    $locationProvider.html5Mode(true);
+});
 
 app.controller("ViewController", ["$scope", function($scope){
 
