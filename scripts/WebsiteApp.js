90095ca8a7c9db203adb417538d45ae6f73fcf92
RyBall <rye5bread5@aol.com>
2014-10-29 18:37:54 -0400

    added pastprojectscontroller

--- scripts/WebsiteApp.js
+++ scripts/WebsiteApp.js
@@ -3,7 +3,7 @@ var app = angular.module("websiteApp", ['ngRoute']);
 app.config(function($routeProvider){
   $routeProvider.
     when('/', {templateUrl:'partials/about.html', controller:'AboutController'}).
-    when('/pastprojects', {templateUrl: 'partials/pastprojects.html'}).
+    when('/pastprojects', {templateUrl: 'partials/pastprojects.html', controller:'PastProjectsController'}).
     when('/currentprojects', {templateUrl: 'partials/currentprojects.html'}).
     when('/futureprojects', {templateUrl: 'partials/futureprojects.html'}).
     when('/skills', {templateUrl: 'partials/skills.html'}).
@@ -20,3 +20,8 @@ app.controller("AboutController", ["$scope", function($scope){
   alert("using about controller");
 
 }]);
+
+app.controller("PastProjectsController", ["$scope", function($scope){
+  alert("using pastprojects controller");
+
+}])
