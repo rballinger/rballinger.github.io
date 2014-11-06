263fecdfcd5d59b29ed2c65d931ab87cf85c7b4e
RyBall <rye5bread5@aol.com>
2014-11-05 20:51:33 -0500

    changed w back to , moved partial scripts into controllers

--- scripts/WebsiteApp.js
+++ scripts/WebsiteApp.js
@@ -25,11 +25,8 @@ app.controller("AboutController", ["$scope", "$window", function($scope, $window
   var small = true;
   var w = angular.element($window);
 
-  w.$('.active').removeClass("active");
-  w.$('#about').addClass("active");
-
-  w.$('.active').removeClass("active");
-  w.$('#about').addClass("active");
+  $window.$('.active').removeClass("active");
+  $window.$('#about').addClass("active");
 
   $scope.slideClick = function(iconName, $event){
     $event.preventDefault();
@@ -38,41 +35,41 @@ app.controller("AboutController", ["$scope", "$window", function($scope, $window
     }
     switch(iconName){
       case "right":
-        w.$("#slide"+currentSlide).toggle("slide", {direction:"left"}, function(){
-          w.$("#slideIcon"+currentSlide).removeClass("fa-dot-circle-o").addClass("fa-circle-o");
+        $window.$("#slide"+currentSlide).toggle("slide", {direction:"left"}, function(){
+          $window.$("#slideIcon"+currentSlide).removeClass("fa-dot-circle-o").addClass("fa-circle-o");
           currentSlide++;
           if(currentSlide > maxSlide)
             currentSlide = 1;
-          w.$("#slide"+currentSlide).toggle("slide", {direction:"right"});
-          w.$("#slideIcon"+currentSlide).removeClass("fa-circle-o").addClass("fa-dot-circle-o");
+          $window.$("#slide"+currentSlide).toggle("slide", {direction:"right"});
+          $window.$("#slideIcon"+currentSlide).removeClass("fa-circle-o").addClass("fa-dot-circle-o");
         });
         break;
       case "left":
-        w.$("#slide"+currentSlide).toggle("slide", {direction:"right"},function(){
-          w.$("#slideIcon"+currentSlide).removeClass("fa-dot-circle-o").addClass("fa-circle-o");
+        $window.$("#slide"+currentSlide).toggle("slide", {direction:"right"},function(){
+          $window.$("#slideIcon"+currentSlide).removeClass("fa-dot-circle-o").addClass("fa-circle-o");
           currentSlide--;
           if(currentSlide < 1)
             currentSlide = maxSlide;
-          w.$("#slide"+currentSlide).toggle("slide", {direction:"left"});
-          w.$("#slideIcon"+currentSlide).removeClass("fa-circle-o").addClass("fa-dot-circle-o");
+          $window.$("#slide"+currentSlide).toggle("slide", {direction:"left"});
+          $window.$("#slideIcon"+currentSlide).removeClass("fa-circle-o").addClass("fa-dot-circle-o");
         });
         break;
       case "1":
       case "2":
       case "3":
         if(iconName > currentSlide){
-          w.$("#slide"+currentSlide).toggle("slide", {direction:"left"}, function(){
-            w.$("#slideIcon"+currentSlide).removeClass("fa-dot-circle-o").addClass("fa-circle-o");
+          $window.$("#slide"+currentSlide).toggle("slide", {direction:"left"}, function(){
+            $window.$("#slideIcon"+currentSlide).removeClass("fa-dot-circle-o").addClass("fa-circle-o");
             currentSlide = iconName;
-            w.$("#slide"+currentSlide).toggle("slide", {direction:"right"});
-            w.$("#slideIcon"+currentSlide).removeClass("fa-circle-o").addClass("fa-dot-circle-o");
+            $window.$("#slide"+currentSlide).toggle("slide", {direction:"right"});
+            $window.$("#slideIcon"+currentSlide).removeClass("fa-circle-o").addClass("fa-dot-circle-o");
           });
         }else if(iconName < currentSlide){
-          w.$("#slide"+currentSlide).toggle("slide", {direction:"right"},function(){
-            w.$("#slideIcon"+currentSlide).removeClass("fa-dot-circle-o").addClass("fa-circle-o");
+          $window.$("#slide"+currentSlide).toggle("slide", {direction:"right"},function(){
+            $window.$("#slideIcon"+currentSlide).removeClass("fa-dot-circle-o").addClass("fa-circle-o");
             currentSlide = iconName;
-            w.$("#slide"+currentSlide).toggle("slide", {direction:"left"});
-            w.$("#slideIcon"+currentSlide).removeClass("fa-circle-o").addClass("fa-dot-circle-o");
+            $window.$("#slide"+currentSlide).toggle("slide", {direction:"left"});
+            $window.$("#slideIcon"+currentSlide).removeClass("fa-circle-o").addClass("fa-dot-circle-o");
           });
         }
         break;
@@ -83,19 +80,19 @@ app.controller("AboutController", ["$scope", "$window", function($scope, $window
   };
 
   function autoslide(){
-    w.$(".glyphicon-circle-arrow-right").click();
+    $window.$(".glyphicon-circle-arrow-right").click();
   };
 
   function resized(){
-    width = w.outerWidth;
+    width = $window.outerWidth;
     if(width < 768){
       if(!small){
-        w.clearInterval(timer);
+        $window.clearInterval(timer);
         small = true;
       }
     }else{
       if(small){
-        timer = w.setInterval(autoslide, delay);
+        timer = $window.setInterval(autoslide, delay);
         small = false;
       }
     }
@@ -107,30 +104,36 @@ app.controller("AboutController", ["$scope", "$window", function($scope, $window
 
 app.controller("PastProjectsController", ["$scope", "$window", function($scope, $window){
   $window.clearInterval(timer);
-  $window.alert("using pastprojects controller");
+  $window.$('.active').removeClass("active");
+  $window.$('#pastprojects').addClass("active");
 }])
 
 app.controller("CurrentProjectsController", ["$scope", "$window", function($scope, $window){
   $window.clearInterval(timer);
-  $window.alert("using currentprojects controller");
+  $window.$('.active').removeClass("active");
+  $window.$('#currentprojects').addClass("active");
 }]);
 
 app.controller("FutureProjectsController", ["$scope", "$window", function($scope, $window){
   $window.clearInterval(timer);
-  $window.alert("using futureprojects controller");
+  $window.$('.active').removeClass("active");
+  $window.$('#futureprojects').addClass("active");
 }]);
 
 app.controller("SkillsController", ["$scope", "$window", function($scope, $window){
   $window.clearInterval(timer);
-  $window.alert("using skills controller");
+  $window.$('.active').removeClass("active");
+  $window.$('#skills').addClass("active");
 }]);
 
 app.controller("BlogController", ["$scope", "$window", function($scope, $window){
   $window.clearInterval(timer);
-  $window.alert("using Blog controller");
+  $window.$('.active').removeClass("active");
+  $window.$('#blog').addClass("active");
 }]);
 
 app.controller("ContactController", ["$scope", "$window", function($scope, $window){
   $window.clearInterval(timer);
-  $window.alert("using contact controller");
+  $window.$('.active').removeClass("active");
+  $window.$('#contact').addClass("active");
 }]);
